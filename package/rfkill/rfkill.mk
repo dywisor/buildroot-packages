17ab03150ea442187d32997983c540029e89ada8
André Erdmann <dywi@mailerd.de>
2014-09-08 06:33:56 +0200

    add package/rfkill
    Tool to read and control rfkill status through /dev/rfkill
    
    Dependency of TLP.

---
+++ package/rfkill/rfkill.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# rfkill
+#
+################################################################################
+
+RFKILL_VERSION       = 0.5
+RFKILL_SOURCE        = rfkill-$(RFKILL_VERSION).tar.xz
+RFKILL_SITE          = https://www.kernel.org/pub/software/network/rfkill
+RFKILL_LICENSE       = ISC
+RFKILL_LICENSE_FILES = COPYING
+
+# version.sh fails with exit code 2 (<< git version suffix error)
+# when not specifying a VERSION_SUFFIX
+define RFKILL_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" \
+		V=1 VERSION_SUFFIX="-br"
+endef
+
+define RFKILL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 -- $(@D)/rfkill $(TARGET_DIR)/usr/sbin/rfkill
+endef
+
+define RFKILL_CLEAN_CMDS
+	rm -f -- $(TARGET_DIR)/usr/sbin/rfkill
+endef
+
+$(eval $(generic-package))
