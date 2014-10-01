dywisor/buildroot-packages

commit 3b208fb75febaec082fd6a27611a6d1e09821508

package/tlp: allow installation if BR2_INIT_NONE=y
Allow custom installation for initramfs usage etc.

package/tlp/tlp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TLP_VERSION         = 0.5.90X
-TLP_LIVEVER_REF     = 3aed0b1051640dcefa7940f8de99e8ee543d02bd
+TLP_LIVEVER_REF     = 8a6d479218f9e55400da06a49405c63682fb86d4
 TLP_SOURCE          = tlp-$(TLP_LIVEVER_REF).tar.gz
 TLP_SITE            = $(call github,linrunner,TLP,$(TLP_LIVEVER_REF))
 TLP_LICENSE         = GPLv2+ GPLv3
@@ -150,6 +150,16 @@ TLP__EDITSRC += MACRO TLP_DEBUG _
 # relocate CONFFILE [optional]
 ifneq ($(call qstrip,$(TLP__CONFFILE)),/etc/default/tlp)
 TLP__EDITSRC += MACRO conffile "$(TLP__CONFFILE)"
+endif
+
+# drop udev files
+ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
+else
+ifeq ($(BR2_INIT_NONE),y)
+TLP__EDITSRC += editmk '/(ln|install).*_ULIB/d'
+else
+$(error "no-udev needs init-none")
+endif
 endif
 
 # POST_BUILD:

package/tlp/Config.in
@@ -1,18 +1,17 @@
+if !BR2_INIT_NONE
 comment "TLP needs udev /dev management"
 	depends on !BR2_PACKAGE_HAS_UDEV
 
 comment "TLP needs systemd" # or pm-utils
 	depends on ! ( BR2_PACKAGE_SYSTEMD )
-
+endif
 
 menuconfig BR2_PACKAGE_TLP
-	bool   "TLP"
+	bool "TLP"
 
-	depends on BR2_PACKAGE_HAS_UDEV
-	depends on ( BR2_PACKAGE_SYSTEMD )
+	depends on BR2_PACKAGE_SYSTEMD || BR2_INIT_NONE
 
-	depends on BR2_LARGEFILE          # hdparm
-
+	depends on BR2_LARGEFILE # hdparm
 	select BR2_PACKAGE_HDPARM
 
 	help
@@ -22,7 +21,6 @@ menuconfig BR2_PACKAGE_TLP
 		for Linux without the need to understand every technical detail.
 		Originally designed for laptops, specifically IBM/Lenovo ThinkPads,
 		but capable to run in various environments.
-
 
 		Its features include:
 
@@ -96,6 +94,9 @@ menuconfig BR2_PACKAGE_TLP
 
 
 if BR2_PACKAGE_TLP
+
+	comment "no init system selected, TLP needs to be run manually"
+		depends on BR2_INIT_NONE
 
 	choice
 		prompt "System Type"
@@ -213,7 +214,7 @@ menu "Features"
 
 	config BR2_PACKAGE_TLP_PCILIST
 		bool "install tlp-pcilist"
-		default y
+		default y if !BR2_INIT_NONE
 
 		depends on !BR2_bfin # pciutils
 		select BR2_PACKAGE_PCIUTILS
@@ -226,7 +227,7 @@ menu "Features"
 
 	config BR2_PACKAGE_TLP_USBLIST
 		bool "install tlp-usblist"
-		default y
+		default y if !BR2_INIT_NONE
 
 		depends on BR2_TOOLCHAIN_HAS_THREADS # usbutils->libusb
 		select BR2_PACKAGE_USBUTILS
@@ -310,14 +311,17 @@ menu "Features"
 			help
 				If set to y, creates a symlink to TLP's tpacpi-bat in /usr/bin.
 
-	comment "tlp-rdw requires NetworkManager and TLP w/ wireless"
-		depends on ! ( BR2_PACKAGE_NETWORK_MANAGER && BR2_PACKAGE_TLP_FEATURE_WIRELESS )
+	comment "tlp-rdw requires NetworkManager, TLP w/ wireless and udev /dev management"
+		depends on ! ( \
+			BR2_PACKAGE_NETWORK_MANAGER && BR2_PACKAGE_TLP_FEATURE_WIRELESS && \
+			BR2_PACKAGE_HAS_UDEV )
 
 	config BR2_PACKAGE_TLP_RDW
 		bool "install tlp-rdw"
 
 		depends on BR2_PACKAGE_TLP_FEATURE_WIRELESS
 		depends on BR2_PACKAGE_NETWORK_MANAGER
+		depends on BR2_PACKAGE_HAS_UDEV
 
 		help
 			Say y to install the Radio Device Wizard.

commit 4ed9a8092c5a30a7b9be0e80ef8ccc2ef13e0516

package/tlp: fix
remove $(error ...) statement, introduced by
3b208fb75febaec082fd6a27611a6d1e09821508

package/tlp/tlp.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TLP_VERSION         = 0.5.90X
-TLP_LIVEVER_REF     = 8a6d479218f9e55400da06a49405c63682fb86d4
+TLP_LIVEVER_REF     = 310a16efd085743780b636babd9d868106f71a9c
 TLP_SOURCE          = tlp-$(TLP_LIVEVER_REF).tar.gz
 TLP_SITE            = $(call github,linrunner,TLP,$(TLP_LIVEVER_REF))
 TLP_LICENSE         = GPLv2+ GPLv3
@@ -157,8 +157,6 @@ ifeq ($(BR2_PACKAGE_HAS_UDEV),y)
 else
 ifeq ($(BR2_INIT_NONE),y)
 TLP__EDITSRC += editmk '/(ln|install).*_ULIB/d'
-else
-$(error "no-udev needs init-none")
 endif
 endif
 